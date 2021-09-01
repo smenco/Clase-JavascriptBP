4c3621e0fdb41e5e69b8f2161e4d3478af672286
Sergio Menco <Sergio Menco>
2021-08-31 19:43:57 -0500

    Hola mundo, variables y constantes

---
+++ script.js
@@ -0,0 +1,36 @@
+//alert('Hello world');
+
+// Variables
+let mensaje;
+
+mensaje = "Hola mundo";
+//alert(mensaje);
+
+mensaje = "Hola mundo dos";
+//alert(mensaje);
+
+mensaje = "Hola mundo tres";
+//alert(mensaje);
+
+let a=3, b=4, c=5;
+console.log(a+b);
+
+let nombre = "Sergio";
+let apellido = "Menco";
+
+console.log("Hola", nombre, apellido);
+
+//Nombrar variables
+
+// Camel Case
+
+let nombreUno = "Sergio";
+let nombreDos = "Menco";
+
+//Nombrar como underscore
+
+let nombre_uno = "Sergio";
+let nombre_dos = "Menco";
+
+//CONSTANTES
+const PI = 3.141624;
